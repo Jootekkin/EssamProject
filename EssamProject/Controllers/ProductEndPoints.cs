cd0a6591c0455616d2f219d2f0e28dbc4c4905f9
Yusuf Mostafa <yusufmustafa1@outlook.com>
2026-09-20 14:51:22 +0300

    Add project files.

---
+++ EssamProject/Controllers/ProductEndPoints.cs
@@ -0,0 +1,23 @@
+﻿namespace EssamProject.Controllers
+{
+    public static class ProductEndPoints
+    {
+        public static RouteGroupBuilder MapProductEndPoints(this IEndpointRouteBuilder group)
+        {
+            var productGroup = group.MapGroup("/api/products");
+            productGroup.MapGet("/list", GetAllProducts);
+
+            return productGroup;
+        }
+
+        private static IResult GetAllProducts(HttpContext context)
+        {
+            return Results.Ok(new List<object>
+            {
+                new  { Id = 1, Name = "Product 1", Price = 10.0m },
+                new  { Id = 2, Name = "Product 2", Price = 20.0m },
+                new  { Id = 3, Name = "Product 3", Price = 30.0m }
+            });
+        }
+    }
+}
