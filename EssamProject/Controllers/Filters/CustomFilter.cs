f5bf8224ff6ccabf670ea9c5fa58fbb1ce588d0a
Yusuf Mostafa <yusufmustafa1@outlook.com>
2026-09-21 17:43:15 +0300

    [Adding Globel Filter and Attribute]

---
+++ EssamProject/Controllers/Filters/CustomFilter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EssamProject.Controllers.Filters
+{
+    public class CustomFilter : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            Console.WriteLine("Execution started.");
+            await next();
+            Console.WriteLine("Execution completed.");
+        }
+    }
+}
---
+++ EssamProject/Controllers/Filters/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EssamProject.Controllers.Filters
+{
+
+    [ApiController]
+    [Route("api/filters")]
+    [CustomFilter] // Apply the custom filter to this controller
+    public class FiltersController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("This is version of the Filters.");
+        }
+    }
+}
