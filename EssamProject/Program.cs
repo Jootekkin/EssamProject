2b4c9c0695bbb4467bc292f34b0634f02761557f
Yusuf Mostafa <yusufmustafa1@outlook.com>
2026-09-23 14:23:18 +0300

    [Add Authentication, Authorization in cookies]

--- EssamProject/Program.cs
+++ EssamProject/Program.cs
@@ -1,5 +1,8 @@
 using EssamProject.Minimal_API;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +23,14 @@ builder.Services.AddApiVersioning(options =>
     options.ApiVersionReader = new MediaTypeApiVersionReader("ver");
 });
 
+builder.Services.AddAuthentication().AddCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 
@@ -34,11 +45,40 @@ if (app.Environment.IsDevelopment())
 app.MapGet("/Welcome", () => "Welcome to first minimal API");
 
 app.MapGroupingMinimal();
+
+app.MapGet("/Login", (HttpContext context) =>
+{
+
+    var claims = new List<Claim>
+    {
+        new Claim(ClaimTypes.Name, "Essam"),
+        new Claim(ClaimTypes.Role, "Admin")
+    };
+
+    var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+
+    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+    context.SignInAsync("Cookies", claimsPrincipal);
+
+    context.Response.WriteAsync("Login successful");
+});
+
+app.MapGet("/Logout", (HttpContext context) =>
+{
+    context.SignOutAsync("Cookies");
+});
+
+app.MapGet("/anonymous", () => "anonymous");
+
+app.MapGet("/secret", [Authorize] () => "secret");
 #endregion
 
 app.UseHsts();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
